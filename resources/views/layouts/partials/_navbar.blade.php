14cafff0ea985e9f5615fa5b42cdbd4995dbff3a
Pierre.F <pierre.forques@viacesi.fr>
2020-06-11 14:49:30 +0200

    auth

--- resources/views/layouts/partials/_navbar.blade.php
+++ resources/views/layouts/partials/_navbar.blade.php
@@ -13,5 +13,4 @@
           </div>
         </div>
       </nav>
-
 </header>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('register', function(){
+    return view('register');
+});
+
+Route::get('login', function(){
+    return view('login');
+});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
