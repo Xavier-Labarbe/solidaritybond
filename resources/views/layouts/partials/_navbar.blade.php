776e2af6cf22fc3a75a8b1fc196adbfdf2aa1ba8
Pierre.F <pierre.forques@viacesi.fr>
2020-06-12 14:10:05 +0200

    correct some conflict

--- resources/views/layouts/partials/_navbar.blade.php
+++ resources/views/layouts/partials/_navbar.blade.php
@@ -1,17 +1,17 @@
-
 <header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand navbar_logo" href="/"><img src="image/logo_fablab.png" width="40px"/></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav">
-            <a class="nav-item nav-link active" href="#">Espace de rendez-vous <span class="sr-only">(current)</span></a>
-            <a class="nav-item nav-link" href="#">Mise en relation</a>
-            <a class="nav-item nav-link" href="#">Calendrier</a>
-            <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">S'inscrire</a>
-          </div>
-        </div>
-      </nav>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand navbar_logo" href="/"><img src="image/logo_fablab.png" width="40px" /></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+      aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-item nav-link active" href="#">Espace de rendez-vous <span class="sr-only">(current)</span></a>
+        <a class="nav-item nav-link" href="conversations">Mise en relation</a>
+        <a class="nav-item nav-link" href="#">Calendrier</a>
+        <a class="nav-item nav-link disabled" href="/register" tabindex="-1" aria-disabled="true">S'inscrire</a>
+      </div>
+    </div>
+  </nav>
 </header>
