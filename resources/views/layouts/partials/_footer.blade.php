7f330391002fb94eed9e3dca738f09dc517cce05
matheoberger <matheo.berger@viacesi.fr>
2020-06-12 20:09:39 +0200

    embeded footer

--- resources/views/layouts/partials/_footer.blade.php
+++ resources/views/layouts/partials/_footer.blade.php
@@ -1,12 +1,19 @@
 <div class="card">
-    <div class="card-header">
-        Footer
-    </div>
-    <div class="card-body">
-        <blockquote class="blockquote mb-0">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-            <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite>
-            </footer>
-        </blockquote>
+    <div class="card-body footer_container">
+
+        <img src="image/logo_fablab.png" width="50px" />
+
+        <div>
+            <i class="fab fa-facebook fa-2x "></i>
+            <i class="fab fa-twitter fa-2x"></i>
+            <i class="fab fa-linkedin-in fa-2x"></i>
+        </div>
+        <div>
+            <p style="text-align: center">Nos sponsors</p>
+            <img src="image/RPVI.png" width="140px" />
+        </div>
+        <a href="/views/legalNotice">Mentions légales</a>
+        <a>Contact</a>
+
     </div>
 </div>
