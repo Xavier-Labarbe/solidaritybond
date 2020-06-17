7c2b542850860ed57e6505689fd2675bbb9bd1c4
Xavier-Labarbe <xavier.labarbe@viacesi.fr>
2020-06-17 15:43:00 +0200

    adding store function in ConversationRepository (api)

--- app/Http/Controllers/Api/ConversationsController.php
+++ app/Http/Controllers/Api/ConversationsController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreMessageRequest;
 use App\Repository\ConversationRepository;
 use App\User;
 use Illuminate\Http\Request;
 
-class ConversationsController extends Controller {
+/**
+ * @property  conversationRepository
+ */
+class ConversationsController extends Controller
+{
 
     /**
      * @var ConversationRepository
@@ -19,17 +24,30 @@ class ConversationsController extends Controller {
         $this->conversationsRepository = $conversationsRepository;
     }
 
-    public function index (Request $request) {
-        return response()
-            ->json([
+    public function index(Request $request)
+    {
+        return [
                 'conversations' => $this->conversationsRepository->getConversations($request->user()->id)
-            ]);
+            ];
     }
 
-    public function show (Request $request, User $user) {
+    public function show(Request $request, User $user)
+    {
         $messages = $this->conversationsRepository->getMessagesFor($request->user()->id, $user->id)->get();
         return [
             'messages' => $messages->reverse()
+        ];
+    }
+
+    public function store(User $user, StoreMessageRequest $request)
+    {
+        $message = $this->conversationsRepository->createMessage(
+            $request->get('content'),
+            $request->user()->id,
+            $user->id
+        );
+        return [
+            'message' => $message
         ];
     }
 }
