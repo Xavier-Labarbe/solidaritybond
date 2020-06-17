60433b75cc180c81c5b4652133f36a99da6e3007
Xavier-Labarbe <xavier.labarbe@viacesi.fr>
2020-06-17 14:36:00 +0200

    implementing isMe function in chat

--- resources/js/components/MessageComponent.vue
+++ resources/js/components/MessageComponent.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
         <div class="row">
-            <div class="col-md-10">
+            <div :class="cls">
                 <p>
-                    <strong>{{ message.from.name }} {{ user }}</strong><br>
+                    <strong>{{ name }}</strong><br>
                     {{ message.content }}
                 </p>
             </div>
@@ -17,6 +17,21 @@
         props: {
             message: Object,
             user: Number
+        },
+        computed: {
+            isMe () {
+                return this.message.from_id === this.user
+            },
+            cls () {
+                let cls = ['col-md-10']
+                if (this.isMe) {
+                    cls.push('offset-md-2 text-right')
+                }
+                return cls
+            },
+            name () {
+               return this.isMe ? 'Moi' : this.message.from.name
+            }
         }
     }
 </script>
