b81569c1c8df91788a17537614af05679d95d810
elouan <elouan@hp-laptop.home>
2020-06-17 14:25:20 +0200

    account page update

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Validator;
+
 use Auth;
 
 use App\User;
@@ -12,9 +14,38 @@ class AccountController extends Controller
 {
     public function index (Request $req)
     {
+        return $this->validator($req);
+        //return view('account');
+    }
+
+    protected function validator(Request $data)
+    {
+         $validate = Validator::make($data->all(), [
+            'Name' => ['required', 'string', 'max:255'],
+            'Firstname' => ['required', 'string', 'max:255'],
+            'Address' => ['required', 'string', 'max:255'],
+            'Email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'Phone' => ['required', 'string', 'max:14'],
+            'Newpassword' => ['string', 'min:8', 'confirmed']
+        ]);
+
+        if ($validate->fails()){
+            return view('account')->withErrors($validate->errors());
+        }
+        else
+        {
+            echo 'ok';
+            //return $this->changeinfo($req);
+        }
+    }
+
+    public function changeinfo (Request $req)
+    {
+        //crypter mot de passe
+        
         User::where('id', Auth::user()->id)
         ->update(['name'=>$req->Name, 'first_name'=>$req->Firstname, 'address' => $req->Address, 'email'=>$req->Email, 'phone'=>$req->Phone,]);
-        //manque mot de passe
-        return view('account');
+        
+        //return view('account');
     }
 }
--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -5,33 +5,82 @@
         <form method="POST" action="{{  route('account') }}">
             @csrf
             <div class="form-group">
-              <label>Nom</label>
-              <input name="Name" class="form-control" value="{{ Auth::user()->name }}">
+                <label>Nom</label>
+                <input name="Name" class="form-control" value="{{ Auth::user()->name }}">
+                @error('Name')
+                    <div class="text-danger">
+                        <small>{{ $errors->first('Name') }}</small>
+                    </div>
+                @enderror
             </div>
+
             <div class="form-group">
                 <label>Prénom</label>
                 <input name="Firstname" class="form-control" value="{{ Auth::user()->first_name }}">
+                @error('Firstname')
+                    <div class="text-danger">
+                        <small>{{ $errors->first('Firstname') }}</small>
+                    </div>
+                @enderror
             </div>
+
             <div class="form-group">
                 <label>Adresse</label>
                 <input name="Address" class="form-control" value="{{ Auth::user()->address }}">
+                @error('Address')
+                    <div class="text-danger">
+                        <small>{{ $errors->first('Address') }}</small>
+                    </div>
+                @enderror
             </div>
+
             <div class="form-group">
                 <label>Adresse Email</label>
                 <input name="Email" type="email" class="form-control" value="{{ Auth::user()->email }}">
+                @error('Email')
+                    <div class="text-danger">
+                        <small>{{ $errors->first('Email') }}</small>
+                    </div>
+                @enderror
             </div>
+
             <div class="form-group">
                 <label>Téléphone</label>
                 <input name="Phone" class="form-control" value="{{ Auth::user()->phone }}">
+                @error('Phone')
+                    <div class="text-danger">
+                        <small>{{ $errors->first('Phone') }}</small>
+                    </div>
+                @enderror
             </div>
+
             <div class="form-group">
-                <label>Mot de passe (NE MARCHE PAS)</label>
-                <input name="Password" class="form-control" value="{{ Auth::user()->password }}">
+                <label>Nouveau mot de passe</label>
+                <input type="password" name="Newpassword" class="form-control">
+            </div>
+
+            <div class="form-group">
+                <label>Confirmer le nouveau mot de passe</label>
+                <input type="password" name="Newpassword_confirmation" class="form-control">
+                @error('Newpassword')
+                    <div class="text-danger">
+                        <small>{{ $errors->first('Newpassword') }}</small>
+                    </div>
+                @enderror
+            </div>
+
+            <div class="form-group">
+                <label>Veuillez taper votre mot de passe avant de sauvegarder</label>
+                <input type="password" name="Password" class="form-control">
+                @error('Password')
+                    <div class="text-danger">
+                        <small>{{ $errors->first('Password') }}</small>
+                    </div>
+                @enderror
             </div>
             <button type="submit" class="btn btn-primary">Sauvegarder</button>
         </form>
     </div>
-
     <div class="card accountmessage">
         <h5 class="card-header">Messages non lus</h5>
         <div>afficher les messages</div>
