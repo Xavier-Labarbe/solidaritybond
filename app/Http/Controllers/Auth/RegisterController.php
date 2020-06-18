0b0e8927173684e8ce99da7a13352f5427d8fd03
matheoberger <matheo.berger@viacesi.fr>
2020-06-18 13:30:16 +0200

    fix status bug

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -74,6 +74,7 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'phone' => $data['phone'],
             'password' => Hash::make($data['password']),
+            'status' => '1',
         ]);
     }
 }
