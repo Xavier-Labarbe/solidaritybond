4b87b04ca9bcbb4669c2a1f5140e3b335f6ec1db
Xavier-Labarbe <xavier.labarbe@viacesi.fr>
2020-06-22 13:36:12 +0200

    adding passport auth and fetching user's info

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
-console.log('salut')
+
+
+
+
+
+
+
+
+
+
+
+
+/**fetch('/api/user', {
+    credentials: 'same-origin',
+    headers: {
+        'X-Requested-With':  'XMLHttpRequest',
+        'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+    }
+})*/
