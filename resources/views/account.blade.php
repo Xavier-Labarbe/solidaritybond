ea8237e3083f496a2ba84819d01d856a711d0c3b
elouan <elouan@hp-laptop.home>
2020-06-24 16:20:31 +0200

    account css page

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -6,7 +6,7 @@
         <form method="POST" action="{{  route('account') }}">
             @csrf
             <div class="row no-gutters">
-                <div class="col-md-6 form-group">
+                <div class="accountinput form-group">
                     <label>Nom</label>
                     <input name="Name" class="accountinput form-control" value="{{ Auth::user()->name }}">
                     @error('Name')
@@ -16,7 +16,7 @@
                     @enderror
                 </div>
 
-                <div class="col-md-6 form-group">
+                <div class="accountinput form-group">
                     <label>Prénom</label>
                     <input name="Firstname" class="form-control" value="{{ Auth::user()->first_name }}">
                     @error('Firstname')
@@ -28,9 +28,9 @@
             </div>
 
             <div class="row no-gutters">
-                <div class="col-md-4 form-group">
+                <div class="accountinput2 form-group">
                     <label>Adresse</label>
-                    <input name="Address" class="accountinput form-control" value="{{ Auth::user()->address }}">
+                    <input name="Address" class="form-control" value="{{ Auth::user()->address }}">
                     @error('Address')
                         <div class="text-danger">
                             <small>{{ $errors->first('Address') }}</small>
@@ -38,9 +38,9 @@
                     @enderror
                 </div>
 
-                <div class="col-md-4 form-group">
+                <div class="accountinput2 form-group">
                     <label>Adresse Email</label>
-                    <input name="Email" type="email" class="accountinput form-control" value="{{ Auth::user()->email }}">
+                    <input name="Email" type="email" class="form-control" value="{{ Auth::user()->email }}">
                     @error('Email')
                         <div class="text-danger">
                             <small>{{ $errors->first('Email') }}</small>
@@ -48,7 +48,7 @@
                     @enderror
                 </div>
 
-                <div class="col-md-4 form-group">
+                <div class="accountinput2 form-group">
                     <label>Téléphone</label>
                     <input name="Phone" class="form-control" value="{{ Auth::user()->phone }}">
                     @error('Phone')
@@ -58,14 +58,14 @@
                     @enderror
                 </div>
             </div>
-            
+
             <div class="row no-gutters">
-                <div class="col-md-6 form-group">
+                <div class="accountinput form-group">
                     <label>Nouveau mot de passe</label>
-                    <input type="password" name="Newpassword" class="accountinput form-control">
+                    <input type="password" name="Newpassword" class="form-control">
                 </div>
 
-                <div class="col-md-6 form-group">
+                <div class="accountinput form-group">
                     <label>Confirmer le nouveau mot de passe</label>
                     <input type="password" name="Newpassword_confirmation" class="form-control">
                     @error('Newpassword')
