6d60481fc52a887576e4e0e4f6429a3684bc71aa
elouan <elouan@hp-laptop.home>
2020-06-29 23:31:53 +0200

    change user seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,12 +11,22 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        for ($i = 0; $i < 10; $i++ ) {
-            \Illuminate\Support\Facades\DB::table('users')->insert([
-                'name' => "JhonDoe$i",
-                "email" => "john$i@doe.fr",
-                'password' => bcrypt('root')
-                ]);
-        }
+        DB::table('users')->insert([
+            'name' => "El Khalfi",
+            'first_name' => "Zeineb",
+            'adress' => "CESI Bordeaux",
+            'email' => "zelkhalfi@cesi.fr",
+            'phone' => "0600000000",
+            'status' => "1",
+            'password' => bcrypt('zeineb')]);
+        
+        DB::table('users')->insert([
+            'name' => "Teillier",
+            'first_name' => "Elea",
+            'adress' => "Fablab CESI",
+            'email' => "eteillier@cesi.fr",
+            'phone' => "0700000000",
+            'status' => "2",
+            'password' => bcrypt('elea')]);
     }
 }
