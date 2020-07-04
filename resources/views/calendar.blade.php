d9cc84fa70c6ce084e3e89591a0ce4c443fab92a
elouan <elouan@hp-laptop.home>
2020-07-04 16:16:25 +0200

    pass to french language

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -48,11 +48,11 @@
       <h4>Vous etes connecté en tant que {{ $userName }} !</h4>
       <h5>Vous etes connecté avec l'email {{ $userEmail }}</h5>
       <p>Vous avez maintenant accès aux options suivantes : </p>
-        <a href="/microsoftCalendar" class="btn btn-primary">Calendar</a>
-        <a href="/appointment" class="btn btn-primary">Appointment</a>
-        <a href="/signout" class="btn btn-danger">Sign Out</a>
+        <a href="/microsoftCalendar" class="btn btn-primary">Calendrier</a>
+        <a href="/appointment" class="btn btn-primary">Prendre un rendez-vous</a>
+        <a href="/signout" class="btn btn-danger">Deconnexion</a>
     @else
-      <a href="/signin" class="btn btn-primary btn-large">Click here to sign in</a>
+      <a href="/signin" class="btn btn-primary btn-large">Connexion a Microsoft Outlook</a>
     @endif
   </div>
 @endsection
--- resources/views/microsoftCalendar.blade.php
+++ resources/views/microsoftCalendar.blade.php
@@ -2,14 +2,14 @@
 
 @section('main')
 <div class="card accountinfo">
-<h1>Calendar</h1>
+<h1>Calendrier</h1>
 <table class="table">
   <thead>
     <tr>
-      <th scope="col">Organizer</th>
-      <th scope="col">Subject</th>
-      <th scope="col">Start</th>
-      <th scope="col">End</th>
+      <th scope="col">Organisateur</th>
+      <th scope="col">sujet</th>
+      <th scope="col">Début</th>
+      <th scope="col">Fin</th>
     </tr>
   </thead>
   <tbody>
@@ -18,8 +18,8 @@
             <tr>
             <td>{{ $event->getOrganizer()->getEmailAddress()->getName() }}</td>
             <td>{{ $event->getSubject() }}</td>
-            <td>{{ \Carbon\Carbon::parse($event->getStart()->getDateTime())->format('n/j/y g:i A') }}</td>
-            <td>{{ \Carbon\Carbon::parse($event->getEnd()->getDateTime())->format('n/j/y g:i A') }}</td>
+            <td>{{ \Carbon\Carbon::parse($event->getStart()->getDateTime())->format('m/d/Y H:i') }}</td>
+            <td>{{ \Carbon\Carbon::parse($event->getEnd()->getDateTime())->format('m/d/Y H:i') }}</td>
             </tr>
         @endforeach
         @endif
